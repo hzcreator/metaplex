f8e432ba66f960e44a31d75fbfb91106e0dcce7d
Henry Zarak <henryzarak@icloud.com>
2022-06-18 23:22:37 -0700

    copy changes home

--- js/packages/web/src/views/home/components/SalesList/index.tsx
+++ js/packages/web/src/views/home/components/SalesList/index.tsx
@@ -44,8 +44,8 @@ export const SalesListView = (props: { collectionMintFilter?: string }) => {
       {!props.collectionMintFilter && (
         <Banner
           src="/main-banner.svg"
-          headingText="The amazing world of Metaplex."
-          subHeadingText="Buy exclusive Metaplex NFTs."
+          headingText="The power of generative art."
+          subHeadingText="Buy exclusive 0x NFTs."
           actionComponent={<HowToBuyModal buttonClassName="secondary-btn" />}
           useBannerBg
         />
